d34d288aabd17de2281404eb63ca87f366444e53
dtoillion <toillion@gmail.com>
2020-02-15 15:42:23 +0100

    moved users into the store, async data for api call

---
+++ pages/users/_id.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>User: {{ user.name }}</p>
+    <v-btn color="primary" nuxt to="/users">{{ buttonText }}</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      buttonText: 'Go back'
+    }
+  },
+  computed: {
+    user() {
+      return this.$store.state.users.find((u) => u.id === this.$route.params.id)
+    }
+  }
+}
+</script>
---
+++ pages/users/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <h1>All Users:</h1>
+      <div v-for="user in $store.state.users" :key="user.id">
+        <nuxt-link :to="`/users/${user.id}`">
+          {{ user.id }}
+          {{ user.name }}
+        </nuxt-link>
+        {{ user.title }}
+        {{ user.quote }}
+      </div>
+    </v-flex>
+    <nuxt-child />
+  </v-layout>
+</template>
+
+<script>
+export default {
+  async fetch({ $axios, store }) {
+    const response = await $axios.get('clients.json')
+    const users = response.data
+    store.commit('SET_USERS', users)
+  }
+}
+</script>
